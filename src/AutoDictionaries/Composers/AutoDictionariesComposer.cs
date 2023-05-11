2e81b6ce73a7f720dbd7c714be55ca8ba20c5cbe
Lantzify <lithvia@gmail.com>
2023-05-11 16:58:40 +0200

    Replaced package.manifest with composer

--- src/AutoDictionaries/Composers/AutoDictionariesComposer.cs
+++ src/AutoDictionaries/Composers/AutoDictionariesComposer.cs
@@ -1,4 +1,6 @@
-﻿using Umbraco.Cms.Core.Composing;
+﻿using Umbraco.Cms.Core.Manifest;
+using Umbraco.Cms.Core.Composing;
+using System.Collections.Generic;
 using AutoDictionaries.Core.Services;
 using Umbraco.Cms.Core.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
@@ -6,13 +8,36 @@ using AutoDictionaries.Core.Services.Interfaces;
 
 namespace AutoDictionaries.Core.Composers
 {
-	public class AutoDictionariesComposer : IComposer
-	{
-		public void Compose(IUmbracoBuilder builder)
-		{
-			builder.Services.AddScoped<IADTemplateService, ADTemplateService>();
-			builder.Services.AddScoped<IADPartialViewService, ADPartialViewService>();
-			builder.Services.AddScoped<IAutoDictionariesService, AutoDictionariesService>();
-		}
-	}
+    public class AutoDictionariesComposer : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+            builder.ManifestFilters().Append<AutoDictionariesManifestFilter>();
+            builder.Services.AddScoped<IADTemplateService, ADTemplateService>();
+            builder.Services.AddScoped<IADPartialViewService, ADPartialViewService>();
+            builder.Services.AddScoped<IAutoDictionariesService, AutoDictionariesService>();
+        }
+    }
+    internal class AutoDictionariesManifestFilter : IManifestFilter
+    {
+        public void Filter(List<PackageManifest> manifests)
+        {
+            manifests.Add(new PackageManifest
+            {
+                PackageName = "AutoDictionaries",
+                Scripts = new[]
+                {
+                    "/App_Plugins/AutoDictionaries/backoffice/autoDictionaries/overview.controller.js",
+                    "/App_Plugins/AutoDictionaries/backoffice/autoDictionaries/edit.controller.js",
+                    "/App_Plugins/AutoDictionaries/backoffice/infiniteEditors/dictionaryItem.edit.controller.js",
+                    "/App_Plugins/AutoDictionaries/backoffice/infiniteEditors/generateDictionaries.controller.js",
+                    "/App_Plugins/AutoDictionaries/backoffice/infiniteEditors/matchDictionary.controller.js"
+                 },
+                Stylesheets = new[]
+                {
+                    "/App_Plugins/AutoDictionaries/css/autoDictionaries.css"
+                }
+            });
+        }
+    }
 }
